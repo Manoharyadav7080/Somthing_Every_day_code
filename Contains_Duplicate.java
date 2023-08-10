905309b9c6b89bf80d07589e4f3aa829abc7901d
Manohar Yadav <113102874+Manoharyadav7080@users.noreply.github.com>
2023-08-10 17:03:09 +0530

    leetcode

---
+++ Contains_Duplicate.java
@@ -0,0 +1,30 @@
+package LeetCode;
+     
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+//Input: nums = [1,2,3,1]
+//Output: true
+
+//Input: nums = [1,2,3,4]
+//Output: false
+
+import java.util.Scanner;
+public class Contains_Duplicate {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		
+		int nums[] =  {1,2,1,3,4};
+		int i = 0;
+		while (i < nums.length) {
+			int num = nums[i];
+			
+		}
+	}
+
+}
